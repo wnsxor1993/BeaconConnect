c888037dea6b89d4e9337e87f837e0df54b6ef4e
wnsxor1993 <https://wnsxor1993@github.com>
2023-01-25 13:30:44 +0900

    feat: 프로젝트 생성 및 gitIgnore 추가

---
+++ BeaconConnect/BeaconConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeaconConnect
+//
+//  Created by Zeto on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
