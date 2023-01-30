958bb4c5ac12aacc55e16e3c1912c2e874ace7b8
wnsxor1993 <https://wnsxor1993@github.com>
2023-01-30 14:06:12 +0900

    feat: 간단 View 추가

--- BeaconConnect/BeaconConnect/GPS/GPSViewController.swift
+++ BeaconConnect/BeaconConnect/GPS/GPSViewController.swift
@@ -10,12 +10,21 @@ import CoreLocation
 
 final class GPSViewController: UIViewController {
     
+    @IBOutlet weak var buttonView: UIView!
+    
     private var locationManger: CLLocationManager = .init()
+    private var clCircularRegion: CLCircularRegion?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.configureProperties()
+        self.configureLocationManager()
+        configureCLCircularRegion()
+        self.configureAction()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.buttonView.layer.cornerRadius = 150
     }
 }
 
@@ -30,10 +39,14 @@ extension GPSViewController: CLLocationManagerDelegate {
             self.showRequestLocationServiceAlert()
             
         case .authorizedAlways, .authorizedWhenInUse:
-            let region = self.makeCLCircularRegion()
-            
             self.locationManger.startUpdatingLocation()
-            self.locationManger.startMonitoring(for: region)
+            
+            if let clCircularRegion {
+                self.locationManger.startMonitoring(for: clCircularRegion)
+                
+            } else {
+                NSLog("None Region")
+            }
             
         @unknown default:
             break
@@ -60,24 +73,53 @@ extension GPSViewController: CLLocationManagerDelegate {
         switch state {
         case .unknown:
             print("unknown")
+            self.buttonView.isUserInteractionEnabled = false
+            self.buttonView.backgroundColor = .gray
             
         case .inside:
             print("inside")
+            self.buttonView.isUserInteractionEnabled = true
+            self.buttonView.backgroundColor = .cyan
             
         case .outside:
             print("outside")
+            self.buttonView.isUserInteractionEnabled = false
+            self.buttonView.backgroundColor = .gray
         }
     }
 }
 
 private extension GPSViewController {
     
-    func configureProperties() {
+    func configureLocationManager() {
         self.locationManger.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManger.delegate = self
         
         self.locationManger.allowsBackgroundLocationUpdates = true
         self.locationManger.pausesLocationUpdatesAutomatically = false
+    }
+    
+    func configureAction() {
+        let tapGesture: UITapGestureRecognizer = .init(target: self, action: #selector(printButtonAction))
+        
+        self.buttonView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    func printButtonAction() {
+        print("출근했습니다")
+        
+        self.locationManger.stopUpdatingLocation()
+        
+        if let clCircularRegion {
+            self.locationManger.stopMonitoring(for: clCircularRegion)
+            
+        } else {
+            NSLog("None Region")
+        }
+        
+        self.buttonView.isUserInteractionEnabled = false
+        self.buttonView.backgroundColor = .gray
     }
     
     // 주소 가져오기
@@ -114,14 +156,14 @@ private extension GPSViewController {
     }
     
     // 지오펜싱 범위 설정 및 CLCircularRegion 인스턴스 반환
-    func makeCLCircularRegion() -> CLCircularRegion {
+    func configureCLCircularRegion() {
         let location: CLLocationCoordinate2D = .init(latitude: 37.40228169999993, longitude: 126.96682999999975)
-        let region: CLCircularRegion = .init(center: location, radius: 500.0, identifier: "Summit-M")
+        let region: CLCircularRegion = .init(center: location, radius: 200.0, identifier: "Summit-M")
         
         region.notifyOnEntry = true
         region.notifyOnExit = true
         
-        return region
+        self.clCircularRegion = region
     }
     
     func showRequestLocationServiceAlert() {
